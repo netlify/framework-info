7a81bfd25e453d2c6c13d44471200f35c01fceb2
ehmicky <ehmicky@gmail.com>
2021-12-03 19:41:23 +0100

    chore!: use pure ES modules

--- scripts/webpack.config.core.js
+++ scripts/webpack.config.core.js
@@ -1,16 +1,30 @@
-const path = require('path')
+import { createRequire } from 'module'
+import { fileURLToPath } from 'url'
 
-module.exports = {
-  entry: path.resolve(`${__dirname}/../src/core.js`),
+// TODO: use `import.meta.resolve()` once it is supported without any
+// experimental flags
+const require = createRequire(import.meta.url)
+const PATH_BROWSERIFY_PATH = require.resolve('path-browserify')
+
+const CORE_FILE = fileURLToPath(new URL('../src/core.js', import.meta.url))
+const DIST_DIR = fileURLToPath(new URL('../dist/', import.meta.url))
+
+const webpackConfig = {
+  entry: CORE_FILE,
   devtool: 'source-map',
   output: {
-    path: path.resolve(`${__dirname}/../dist`),
+    path: DIST_DIR,
     filename: 'index.js',
-    library: 'frameworkInfo',
-    libraryTarget: 'umd',
-    globalObject: 'this',
+    library: {
+      type: 'module',
+    },
+  },
+  experiments: {
+    outputModule: true,
   },
   resolve: {
-    fallback: { path: require.resolve('path-browserify') },
+    fallback: { path: PATH_BROWSERIFY_PATH },
   },
 }
+
+export default webpackConfig
